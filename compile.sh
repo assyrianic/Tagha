0af9a095bf2adf39e1f1598aa00544e114f5cb4e
assyrianic <assyrianic@users.noreply.github.com>
2017-08-25 20:28:31 -0500

    uploading missing compile params

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-clang-3.5	-g -std=c11 vmapi.c vm.c hostapp.c -o TaghaVM_Clang
-gcc			-g -std=c11 vmapi.c vm.c hostapp.c -o TaghaVM_GCC
+clang-3.5	-std=c11 -Os hashmap.c vmapi.c vm.c hostapp.c -o TaghaVM_Clang
+gcc			-std=c11 -Os hashmap.c vmapi.c vm.c hostapp.c -o TaghaVM_GCC
 #-S for asm output, -g for debug, -Os for optimization by size		-pg profiling
 # -Wall
 
