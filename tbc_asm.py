7e64b577738cf8b82ed83a1441b1ad4ec706602a
assyrianic <assyrianic@users.noreply.github.com>
2017-08-26 01:32:32 -0500

    uploading python version of assembler
    thought it'd be simpler to make an assembler in Python3. Current it assembles just the test native but it works.

---
+++ tbc_asm.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+
+def enum(*sequential, **named):
+	enums = dict(zip(sequential, range(len(sequential))), **named);
+	return type('Enum', (), enums);
+	
+opcodes = enum('halt', 'pushq', 'pushl', 'pushs', 'pushb', 'pushsp', 'puship', 'pushbp', 'pushspadd', 'pushspsub', 'pushbpadd', 'pushbpsub', 'popq', 'popl', 'pops', 'popb', 'popsp', 'popip', 'popbp', 'wrtq', 'wrtl', 'wrts', 'wrtb', 'storeq', 'storel', 'stores', 'storeb', 'storeqa', 'storela', 'storesa', 'storeba', 'storespq', 'storespl', 'storesps', 'storespb', 'loadq', 'loadl', 'loads', 'loadb', 'loadqa', 'loadla', 'loadsa', 'loadba', 'loadspq', 'loadspl', 'loadsps', 'loadspb', 'copyq', 'copyl', 'copys', 'copyb', 'addq', 'uaddq', 'addl', 'uaddl', 'addf', 'subq', 'usubq', 'subl', 'usubl', 'subf', 'mulq', 'umulq', 'mull', 'umull', 'mulf', 'divq', 'udivq', 'divl', 'udivl', 'divf', 'modq', 'umodq', 'modl', 'umodl', 'addf64', 'subf64', 'mulf64', 'divf64', 'andl', 'orl', 'xorl', 'notl', 'shll', 'shrl', 'andq', 'orq', 'xorq', 'notq', 'shlq', 'shrq', 'incq', 'incl', 'incf', 'decq', 'decl', 'decf', 'negq', 'negl', 'negf', 'incf64', 'decf64', 'negf64', 'ltq', 'ltl', 'ultq', 'ultl', 'ltf', 'gtq', 'gtl', 'ugtq', 'ugtl', 'gtf', 'cmpq', 'cmpl', 'ucmpq', 'ucmpl', 'compf', 'leqq', 'uleqq', 'leql', 'uleql', 'leqf', 'geqq', 'ugeqq', 'geql', 'ugeql', 'geqf', 'ltf64', 'gtf64', 'cmpf64', 'leqf64', 'geqf64', 'neqq', 'uneqq', 'neql', 'uneql', 'neqf', 'neqf64', 'jmp', 'jzq', 'jnzq', 'jzl', 'jnzl', 'call', 'calls', 'calla', 'ret', 'retx', 'reset', 'wrtnataddr', 'pushnataddr', 'callnat', 'callnats', 'callnata', 'mmxaddl', 'mmxuaddl', 'mmxaddf', 'mmxadds', 'mmxuadds', 'mmxaddb', 'mmxuaddb', 'mmxsubl', 'mmxusubl', 'mmxsubf', 'mmxsubs', 'mmxusubs', 'mmxsubb', 'mmxusubb', 'mmxmull', 'mmxumull', 'mmxmulf', 'mmxmuls', 'mmxumuls', 'mmxmulb', 'mmxumulb', 'mmxdivl', 'mmxudivl', 'mmxdivf', 'mmxdivs', 'mmxudivs', 'mmxdivb', 'mmxudivb', 'mmxmodl', 'mmxumodl', 'mmxmods', 'mmxumods', 'mmxmodb', 'mmxumodb', 'mmxandl', 'mmxands', 'mmxandb', 'mmxorl', 'mmxors', 'mmxorb', 'mmxxorl', 'mmxxors', 'mmxxorb', 'mmxnotl', 'mmxnots', 'mmxnotb', 'mmxshll', 'mmxshls', 'mmxshlb', 'mmxshrl', 'mmxshrs', 'mmxshrb', 'mmxincl', 'mmxincf', 'mmxincs', 'mmxincb', 'mmxdecl', 'mmxdecf', 'mmxdecs', 'mmxdecb', 'mmxnegl', 'mmxnegf', 'mmxnegs', 'mmxnegb', 'nop');
+
+print(opcodes.nop);
+
+'''
+bytecode test_native = {
+		27,0,0,0,	// 2-5
+		0,0,0,0,	// set memory size.
+		16,0,0,0,	// set stack size.
+		1,0,0,0,	// set amount of natives!
+		5,0,0,0,	't','e','s','t',0,	// string size of 1st native
+		pushl,	0,0,0,50,	// ammo
+		pushl,	0,0,0,100,	// health
+		pushl,	67,150,0,0,	// speed
+		callnat, 0,0,0,0, 0,0,0,12, 0,0,0,1,	// #1 - get native name, #2 - bytes to push, #3 - number of args
+		halt
+	};
+'''
+
+def make_header(f, entryaddr:int, memsize:int, stksize:int, nativelen:int):
+	f.write(0xC0DE.to_bytes(2, byteorder='little'));
+	f.write(entryaddr.to_bytes(4, byteorder='little'));
+	f.write(memsize.to_bytes(4, byteorder='little'));
+	f.write(stksize.to_bytes(4, byteorder='little'));
+	f.write(nativelen.to_bytes(4, byteorder='little'));
+
+def write_header_natives(f, *natives):
+	i = 0
+	while i<len(natives):
+		#f.write(natives[i].to_bytes(4, byteorder='little'));
+		print((len(natives[i])+1).to_bytes(4, byteorder='little'));
+		f.write((len(natives[i])+1).to_bytes(4, byteorder='little'));
+		f.write(natives[i].encode('utf-8'));
+		f.write(0x0.to_bytes(1, byteorder='little'));
+		i += 1;
+
+
+with open('pyNative.tbc', 'wb') as bytecode:
+	make_header(bytecode, 27, 0, 16, 1);
+	write_header_natives(bytecode, 'test');
+	bytecode.write(opcodes.pushl.to_bytes(1, byteorder='big'));
+	bytecode.write(0x32.to_bytes(4, byteorder='big'));
+	bytecode.write(opcodes.pushl.to_bytes(1, byteorder='big'));
+	bytecode.write(0x64.to_bytes(4, byteorder='big'));
+	bytecode.write(opcodes.pushl.to_bytes(1, byteorder='big'));
+	bytecode.write(0x43960000.to_bytes(4, byteorder='big'));
+	bytecode.write(opcodes.callnat.to_bytes(1, byteorder='big'));
+	bytecode.write(0x0.to_bytes(4, byteorder='big'));
+	bytecode.write(0xc.to_bytes(4, byteorder='big'));
+	bytecode.write(0x1.to_bytes(4, byteorder='big'));
+	bytecode.write(opcodes.halt.to_bytes(1, byteorder='big'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
