398de5cca982719c3a05502d254150e905bf5762
assyrianic <assyrianic@users.noreply.github.com>
2018-11-12 15:18:39 -0700

    Adding Export define to C++ Tagha interface method implementations.

--- tagha_api_cpp.cpp
+++ tagha_api_cpp.cpp
@@ -1,62 +1,62 @@
 #include "tagha.h"
 
-CTagha::CTagha(void *script)
+TAGHA_EXPORT CTagha::CTagha(void *script)
 {
 	Tagha_Init((struct Tagha *)this, script);
 }
 
-CTagha::CTagha(void *script, const struct CNativeInfo natives[])
+TAGHA_EXPORT CTagha::CTagha(void *script, const struct CNativeInfo natives[])
 {
 	Tagha_Init((struct Tagha *)this, script);
 	Tagha_RegisterNatives((struct Tagha *)this, (const struct NativeInfo *)natives);
 }
 
-CTagha::~CTagha()
+TAGHA_EXPORT CTagha::~CTagha()
 {
 	
 }
 
-bool CTagha::RegisterNatives(const struct CNativeInfo natives[])
+TAGHA_EXPORT bool CTagha::RegisterNatives(const struct CNativeInfo natives[])
 {
 	return Tagha_RegisterNatives((struct Tagha *)this, (const struct NativeInfo *)natives);
 }
 
-void *CTagha::GetGlobalVarByName(const char varname[])
+TAGHA_EXPORT void *CTagha::GetGlobalVarByName(const char varname[])
 {
 	return Tagha_GetGlobalVarByName((struct Tagha *)this, varname);
 }
 
-int32_t CTagha::CallFunc(const char funcname[], const size_t args, union TaghaVal params[])
+TAGHA_EXPORT int32_t CTagha::CallFunc(const char funcname[], const size_t args, union TaghaVal params[])
 {
 	return Tagha_CallFunc((struct Tagha *)this, funcname, args, params);
 }
 
-union TaghaVal CTagha::GetReturnValue()
+TAGHA_EXPORT union TaghaVal CTagha::GetReturnValue()
 {
 	return Tagha_GetReturnValue((struct Tagha *)this);
 }
 
-int32_t CTagha::RunScript(int32_t argc, char *argv[])
+TAGHA_EXPORT int32_t CTagha::RunScript(int32_t argc, char *argv[])
 {
 	return Tagha_RunScript((struct Tagha *)this, argc, argv);
 }
 
-const char *CTagha::GetError()
+TAGHA_EXPORT const char *CTagha::GetError()
 {
 	return Tagha_GetError((struct Tagha *)this);
 }
 
-void CTagha::PrintVMState()
+TAGHA_EXPORT void CTagha::PrintVMState()
 {
 	Tagha_PrintVMState((struct Tagha *)this);
 }
 
-void *CTagha::GetRawScriptPtr()
+TAGHA_EXPORT void *CTagha::GetRawScriptPtr()
 {
 	return Tagha_GetRawScriptPtr((struct Tagha *)this);
 }
 
-void CTagha::ThrowError(const int32_t err)
+TAGHA_EXPORT void CTagha::ThrowError(const int32_t err)
 {
 	Tagha_ThrowError((struct Tagha *)this, err);
 }
