17024fc06ae8d4cf2bf769fc8a87438e73250455
assyrianic <assyrianic@users.noreply.github.com>
2018-12-03 21:31:23 -0700

    Delete build_tagha_assembler.sh

--- build_tagha_assembler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-gcc -Wall -Wextra -O2 -std=c99 -s libharbol/stringobj.c libharbol/vector.c libharbol/hashmap.c libharbol/bytebuffer.c libharbol/linkmap.c tagha_assembler/tagha_assembler.c -o tagha_asm
