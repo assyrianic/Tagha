321ea5e1a4a5a26d419e8f1f41f26a1cf3584abd
assyrianic <assyrianic@users.noreply.github.com>
2018-12-03 21:29:59 -0700

    Delete build_tagha_testapps.sh

--- tagha_testcode/build_tagha_testapps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-gcc -Wall -Wextra -std=c99 -s -O2 test_hostapp.c -L. -ltagha -o taghavm_hosttest
-g++ -Wall -Wextra -std=c++17 -s -O2 test_hostapp.cpp -L. -ltaghacpp -ltagha -o taghavm_hosttest_cpp
-#clang-6.0 -Wall -Wextra -std=c99 -O2 test_hostapp.c -L. -ltaghaclang -o taghavmclang
-#clang-6.0 -S -emit-llvm tagha_api.c
-
