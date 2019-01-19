52b0bfd61a227b6ed3fb2759c55182fe4c2631b1
4pplecracker <4pplecracker@gmail.com>
2019-01-19 23:56:48 +0100

    windows build support

---
+++ build_tagha_testapp.bat
@@ -0,0 +1,10 @@
+set CC=clang
+set CFLAGS=-Wextra -Wall -std=c99 -s -O2
+
+%CC% %CFLAGS% -c libharbol/stringobj.c libharbol/vector.c libharbol/hashmap.c libharbol/mempool.c libharbol/linkmap.c tagha_api.c
+
+ar cr libtagha.a stringobj.o vector.o hashmap.o mempool.o linkmap.o tagha_api.o
+
+%CC% %CFLAGS% test_hostapp.c -L. -llibtagha -o taghavmgcc_hosttest.exe
+
+del *.o
