7ae74a49199afb590da48622ba3181b51b44daec
Kevin Yonan <assyrianic@users.noreply.github.com>
2019-02-11 15:13:33 -0700

    Update build_tagha_testapp.bat

--- build_tagha_testapp.bat
+++ build_tagha_testapp.bat
@@ -5,6 +5,6 @@ set CFLAGS=-Wextra -Wall -std=c99 -s -O2
 
 ar cr libtagha.a stringobj.o vector.o hashmap.o mempool.o linkmap.o tagha_api.o
 
-%CC% %CFLAGS% test_hostapp.c -L. -llibtagha -o taghavmgcc_hosttest.exe
+%CC% %CFLAGS% test_hostapp.c -L. -llibtagha -llibtagha_libc -o taghavmgcc_hosttest.exe
 
 del *.o
