83ffd91f716cc11ccb02397212d0c8e08f129d5e
Kevin Yonan <assyrianic@users.noreply.github.com>
2019-02-20 13:47:25 -0700

    make bat file fix
    Credit to @Noxabellus
    - fixed the bat file to successfully compile on windows.

--- make.bat
+++ make.bat
@@ -1,12 +1,11 @@
 @echo off
 
-set CC=clang
-set CFLAGS=-Wextra -Wall -std=c99 -s -O2
-set TESTFLAGS=-Wextra -Wall -std=c99 -g -O2
+set CC=clang-cl
+set CFLAGS=-Xclang -std=c99 -Xclang -O2 -D_CRT_SECURE_NO_WARNINGS
 
 set SRCS=tagha_api.c
 
-set OBJS=%SRCS:.c=.o%
+set OBJS=%SRCS:.c=.obj%
 set LIBNAME=libtagha
 
 set ACTION=%1
@@ -16,8 +15,8 @@ goto %ACTION%
 
 :tagha
        %CC% %CFLAGS% -c libharbol/stringobj.c libharbol/vector.c libharbol/hashmap.c libharbol/mempool.c libharbol/linkmap.c %SRCS%
-       llvm-ar cr %LIBNAME%.lib stringobj.o vector.o hashmap.o mempool.o linkmap.o %OBJS%
-       %CC% -shared stringobj.o vector.o hashmap.o mempool.o linkmap.o %OBJS% -o %LIBNAME%.dll
+       llvm-ar cr %LIBNAME%.lib stringobj.obj vector.obj hashmap.obj mempool.obj linkmap.obj %OBJS%
+       %CC% %CFLAGS% /LD stringobj.obj vector.obj hashmap.obj mempool.obj linkmap.obj %OBJS% -o%LIBNAME%
        goto END
 
 :tagha_asm
@@ -25,8 +24,20 @@ goto %ACTION%
        goto END
 
 :clean
-       del *.o
+       del *.obj
        goto END
+
+:tagha_libc
+	%CC% %CFLAGS% -c ^
+       libharbol/stringobj.c libharbol/vector.c libharbol/hashmap.c libharbol/mempool.c libharbol/linkmap.c ^
+       tagha_libc/tagha_ctype.c tagha_libc/tagha_stdio.c tagha_libc/tagha_stdlib.c tagha_libc/tagha_string.c tagha_libc/tagha_time.c tagha_libc/tagha_module.c
+	
+	llvm-ar cr %LIBNAME%_libc.lib stringobj.obj vector.obj hashmap.obj mempool.obj linkmap.obj tagha_ctype.obj tagha_stdio.obj tagha_stdlib.obj tagha_string.obj tagha_time.obj tagha_module.obj
+	
+	%CC% %CFLAGS% /LD stringobj.obj vector.obj hashmap.obj mempool.obj linkmap.obj tagha_ctype.obj tagha_stdio.obj tagha_stdlib.obj tagha_string.obj tagha_time.obj tagha_module.obj %LIBNAME%.lib -o%LIBNAME%_libc
+
+:testapp
+	%CC% %CFLAGS% test_hostapp.c %LIBNAME%.lib %LIBNAME%_libc.lib -otagha_testapp
 
 :END
 echo %ACTION% is done
