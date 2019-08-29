ec2c0af888f1659f1aead9be9b847d1fac7a37c1
Kevin Yonan <assyrianic@users.noreply.github.com>
2019-08-29 15:54:06 -0700

    Delete compile.sh

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-clang-3.5	-std=c11 vmapi.c vm.c hostapp.c -o TaghaVM_Clang
-gcc			-std=c11 vmapi.c vm.c hostapp.c -o TaghaVM_GCC
-#-S for asm output, -g for debug, -Os for optimization by size		-pg profiling
-# -Wall
-
-#./TaghaVM_GCC
-#gprof TaghaVM_GCC gmon.out > Tagha_profile.txt
